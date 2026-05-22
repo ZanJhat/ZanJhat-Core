ea2de8a5edeb7efaf0ae366020690f692886516a
ZanJhat <vannhatle123@gmail.com>
2026-05-23 00:53:28 +0700

    Add files via upload

---
+++ ZanJhat.Core/Settings/ControlsSettings.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Xml.Linq;
+using Engine;
+using Engine.Serialization;
+using TemplatesDatabase;
+using XmlUtilities;
+using System.Collections.Generic;
+using System.Globalization;
+using Game;
+
+namespace ZanJhat.Core
+{
+    public class ControlsSettings
+    {
+        public bool DoubleTapDash { get; set; } = true;
+    }
+}
